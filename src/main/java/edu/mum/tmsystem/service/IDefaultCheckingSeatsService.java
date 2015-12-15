958b80a7d558c6bdba809308c61a347abb80f4a7
Sanjay Paudel <sanjaypaudel007@gmail.com>
2015-12-15 16:17:46 -0600

    Added method for getting DefaultCheckingSeats By CheckingType in service and repository layer.

--- src/main/java/edu/mum/tmsystem/service/IDefaultCheckingSeatsService.java
+++ src/main/java/edu/mum/tmsystem/service/IDefaultCheckingSeatsService.java
@@ -1,9 +1,12 @@
 package edu.mum.tmsystem.service;
 
 import edu.mum.tmsystem.domain.DefaultCheckingSeats;
+import edu.mum.tmsystem.enums.CheckingType;
 
 public interface IDefaultCheckingSeatsService {
 
 	public void saveCheckingSeats(DefaultCheckingSeats checkingSeats);
+	
+	public DefaultCheckingSeats getDefaultCheckingSeatsByCheckingType(CheckingType checkingType);
 
 }
