d1dfb3f24956e493b747a2f6422f740a2bf394fe
Sanjay Paudel <sanjaypaudel007@gmail.com>
2015-12-15 16:08:19 -0600

    Added method findTMCheckerByUserId in TM Checker Repository.

--- src/main/java/edu/mum/tmsystem/repository/ITMCheckerRepository.java
+++ src/main/java/edu/mum/tmsystem/repository/ITMCheckerRepository.java
@@ -1,11 +1,17 @@
 package edu.mum.tmsystem.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import edu.mum.tmsystem.domain.TMChecker;
 import edu.mum.tmsystem.domain.TMHistory;
 
 @Repository
-public interface ITMCheckerRepository extends CrudRepository<TMHistory, Long>{
+public interface ITMCheckerRepository extends CrudRepository<TMHistory, Long> {
+	
+	@Query("SELECT t FROM TMChecker WHERE t.user.id= : userId")
+	TMChecker findTMCheckerByUserId(@Param("userID") Long id);
 
 }
