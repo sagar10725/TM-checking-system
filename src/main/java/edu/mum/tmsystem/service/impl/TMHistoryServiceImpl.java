af2a93becdd0551aaaff6449054e8806e91405e4
Sanjay Paudel <sanjaypaudel007@gmail.com>
2015-12-16 23:38:45 -0600

    Added table in my signups. Modifying tm history to set tm checker.. still to do

--- src/main/java/edu/mum/tmsystem/service/impl/TMHistoryServiceImpl.java
+++ src/main/java/edu/mum/tmsystem/service/impl/TMHistoryServiceImpl.java
@@ -22,27 +22,27 @@ import edu.mum.tmsystem.util.Utility;
 
 @Service
 @Transactional
-public class TMHistoryServiceImpl implements ITMHistoryService{
+public class TMHistoryServiceImpl implements ITMHistoryService {
 
 	@Autowired
 	ITMHistoryRepository tmHistoryRepository;
-	
+
 	@Autowired
 	IStudentService studentService;
-	
+
 	@Autowired
 	IRoomService roomService;
-	
+
 	@Autowired
 	ITMCheckerService tmCheckerService;
-	
+
 	@Autowired
 	IUserService userService;
-	
+
 	@Override
 	public List<TMHistory> getAllHistory() {
 		// TODO Auto-generated method stub
-		return (List<TMHistory>) tmHistoryRepository.findAll() ;
+		return (List<TMHistory>) tmHistoryRepository.findAll();
 	}
 
 	@Override
@@ -63,26 +63,34 @@ public class TMHistoryServiceImpl implements ITMHistoryService{
 
 	@Override
 	public void addNewChecking(TMHistory tmHistory) {
-		
-		Student student = studentService.getStudentById(tmHistory.getStudent().getId());
+
+		Student student = studentService.getStudentById(tmHistory.getStudent()
+				.getId());
 		tmHistory.setStudent(student);
 		Room room = roomService.getRoomById(tmHistory.getRoom().getId());
 		tmHistory.setRoom(room);
 		tmHistory.setStatus(TMStatusType.CHECKED);
 		tmHistoryRepository.save(tmHistory);
-		User loggedUser = userService.getUserByUsername(Utility.getLoggedInUserName());
-		TMChecker tmchecker = tmCheckerService.getTMCheckerFromUserID(loggedUser.getId());
+		User loggedUser = userService.getUserByUsername(Utility
+				.getLoggedInUserName());
+		TMChecker tmchecker = tmCheckerService
+				.getTMCheckerFromUserID(loggedUser.getId());
 		tmHistory.setCheckedBy(tmchecker);
 		tmHistoryRepository.save(tmHistory);
-		
+
 	}
 
 	@Override
 	public void changeStatus(Integer id, TMStatusType status) {
 		TMHistory tmHistory = tmHistoryRepository.findOne(id);
 		tmHistory.setStatus(status);
+		User loggedUser = userService.getUserByUsername(Utility
+				.getLoggedInUserName());
+		TMChecker tmchecker = tmCheckerService
+				.getTMCheckerFromUserID(loggedUser.getId());
+		tmHistory.setCheckedBy(tmchecker);
 		tmHistoryRepository.save(tmHistory);
-		
+
 	}
 
 }
